738938a524f0bc40b62301038439d05169120805
JhulienyB <jhulienybucci@gmail.com>
2024-05-29 11:59:43 -0300

    Segunda Etapa — Consumo do Backend da Farmácia

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import './index.css';
 import Home from './pages/home/Home';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
+import ListarCategorias from './components/categorias/listarcategorias/ListarCategorias';
+import FormCategoria from './components/categorias/formcategoria/FormCategoria';
+import DeletarCategoria from './components/categorias/deletarcategorias/DeletarCategoria';
 
 function App() {
   return (
@@ -9,7 +12,11 @@ function App() {
     <Navbar/>
     <Home/>
     <Footer/>
+    <ListarCategorias />
+    {/* <FormCategoria /> */}
+    {/* <DeletarCategoria /> */}
     </>
+    
 );
 }
 export default App;
